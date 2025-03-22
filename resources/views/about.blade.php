89dbcd77d0bf0ed8c2d2dcb70d56ff23e601a008
leonardojulius <leonardojulius.jcl@gmail.com>
2025-03-23 01:44:47 +0800

    Reinstall using default sqlite3

---
+++ resources/views/about.blade.php
@@ -0,0 +1,6 @@
+<h1>
+    Hello, Welcome to my laravel course
+</h1>
+<p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, quo sed praesentium accusamus asperiores vero optio possimus eum incidunt blanditiis perspiciatis fugit exercitationem veniam harum laborum sunt ea libero fugiat.
+</p>
