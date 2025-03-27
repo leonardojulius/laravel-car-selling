c874d859a14deb093fac40ce52e6b42df795db5c
leonardojulius <leonardojulius.jcl@gmail.com>
2025-03-27 08:16:19 +0800

    Updating Notes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\CarController;
+use App\Http\Controllers\OperationController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ShowCarController;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -8,4 +13,6 @@ Route::get('/', function () {
 
 
 Route::view('/about', 'about')->name('about');
+
+
 
